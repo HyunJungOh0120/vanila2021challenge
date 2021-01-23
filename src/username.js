f8150643e78227ddad7c510806522e31f4077871
HyunJungOh0120 <72025960+HyunJungOh0120@users.noreply.github.com>
2021-01-23 22:32:31 +0800

    Update username.js

--- src/username.js
+++ src/username.js
@@ -3,21 +3,42 @@ const nameForm = nameContainer.querySelector(".username__form");
 const nameInput = nameForm.querySelector(".username__input");
 const submitBtn = nameForm.querySelector(".username__button");
 
-const nameWritten = nameContainer.querySelector(".username__name");
+const nameCard = nameContainer.querySelector(".username__name");
 
 const USER_LS = "username";
 
 ////////////////////////////////////////////////////////////////////
+const saveName = function (text) {
+  localStorage.setItem(USER_LS, text);
+};
+
+const renderName = function (text) {
+  nameCard.textContent = `HI, ${text} 😁`;
+
+  nameForm.classList.add("hidden");
+  nameCard.classList.remove("hidden");
+};
+
+const askName = function (e) {
+  e.preventDefault();
+
+  const currName = nameInput.value.toUpperCase();
+  renderName(currName);
+  saveName(currName);
+};
 
 ////////////////////////////////////////////////////////////////////
 function init() {
   const loadedName = localStorage.getItem(USER_LS);
   /// 만약에 이름이 있다면? 로컬에?
   if (loadedName) {
+    console.log(typeof loadedName);
+    renderName(loadedName);
   }
   /// 로컬에 이름이 없다면?
   else {
     nameForm.addEventListener("submit", askName);
+    submitBtn.addEventListener("click", askName);
   }
 }
 
